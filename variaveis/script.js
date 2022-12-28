3fc2e72e6ad319108853e051e3f92ab94a038d3f
Haroldo-Mayworm <Haroldomayworm@gmail.com>
2022-12-28 17:18:15 -0300

    commit - initial / variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,49 @@
+var nome = 'Haroldo',
+    idade = 21,
+    maiorIdade = true;
+// console.log(nome, idade, maiorIdade);
+
+
+var preco = 35,
+    quantComprada = 4,
+    precoTotal = quantComprada*preco;
+// console.log(precoTotal);
+
+
+var semDefinir;
+// console.log(semDefinir);
+
+
+// console.log(faltaDefinir)
+
+
+idade = 40
+// console.log(idade);
+
+
+let time = 'vasco';
+
+time = 'flamengo';
+// console.log(time);
+
+
+// EXERCICIOS
+
+// Declarar uma variável com o seu nome
+var nomeCompleto = 'Haroldo Mayworm';
+
+// Declarar uma variável com a sua idade
+var idadeHaroldo = 21;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+var comidaFavorita
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'feijoada'
+
+// Declarar 5 variáveis diferentes sem valores
+var cor,
+    peso,
+    altura,
+    livroFavorito,
+    filmeFavorito;
