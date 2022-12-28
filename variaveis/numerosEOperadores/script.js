1a69342d64ec30e84cd3c848967e4888825200de
Haroldo-Mayworm <Haroldomayworm@gmail.com>
2022-12-28 17:28:22 -0300

    commit - numerosEOperadores

---
+++ variaveis/numerosEOperadores/script.js
@@ -0,0 +1,54 @@
+var gols = 100,
+    pi = 3.14,
+    exp = 5e10;
+// console.log(exp);
+
+// operadores Aritméticos
+var soma = 2+2,
+    subtracao = 8-3,
+    multiplicacao = 20*5,
+    divisao = 80/2,
+    expoente = 4**2,
+    modulo = 14%5;
+// console.log(modulo);
+
+
+var soma2 = '50'+50,
+    subtracao2 = '100' - 20,
+    multiplicacao2 = '10'*3,
+    divisao2 = '10'/2,
+    divisao3 = 'comprei 10'*2;
+// console.log(divisao3);
+// console.log(isNaN(divisao3));
+
+var teste1 = 10+10,
+    teste2 = 10+10/2,
+    teste3 = (10+10)/2,
+    teste4 = 10*10/5,
+    teste5 = 20/2*5;
+// console.log(teste5);
+
+
+var idadeTeste = '50',
+    x = 5
+// console.log(+idadeTeste + x);
+// console.log(-idadeTeste + x);
+
+
+// EXERCICIOS
+
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+// console.log(total); // 35
+
+// Crie duas expressões que retornem NaN
+var subtracaoNaN = 'isso 100'-10,
+    divisaoNaN = 'aquilo 50'/2;
+// console.log(divisaoNaN);
+
+// Somar a string '200' com o número 50 e retornar 250
+// console.log(+'200' + 50);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var x = 5;
+// console.log(++x);
