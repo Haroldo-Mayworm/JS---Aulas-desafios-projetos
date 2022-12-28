c2c984803f3261fcbb55dc14a25328cc9f55163e
Haroldo-Mayworm <Haroldomayworm@gmail.com>
2022-12-28 17:27:35 -0300

    commit - tipoDadosVariaveis

---
+++ variaveis/tiposDeDados/script.js
@@ -0,0 +1,64 @@
+// String
+var estado = 'Rio de Janeiro'
+
+// Number
+var idade = 21;
+
+// Boolean
+var maiorIdade = true;
+
+// Undefined
+var filmeFavorito;
+
+// Null
+var corFavorita = null;
+
+// Symbol
+var simbolo = Symbol();
+
+// Object
+var novoObjeto = {};
+// console.log(typeof maiorIdade);
+
+
+var nome = 'haroldo',
+    sobrenome = 'mayworm';
+var nomeCompleto = nome + ' ' + sobrenome;
+// console.log(nomeCompleto);
+
+
+var gols = 1000;
+var frase = 'Pele fez mais de '+gols+' gols!'
+// console.log(frase);
+
+
+var defesas = 400;
+var frase2 = `Rogerio Ceni defendeu mais de ${defesas*2} vezes!`
+// console.log(frase2);
+
+
+var frase3 = 'Esse é o \"melhor\" esporte!';
+// console.log(frase3);
+
+
+// EXERCICIOS
+
+// Declare uma variável contendo uma string
+var timeDeCoracao = 'flamengo';
+
+// Declare uma variável contendo um número dentro de uma string
+var anoNascimento = '2001';
+
+// Declare uma variável com a sua idade
+var idadeExerc = 21;
+
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+var primeiroNome = 'Haroldo',
+    ultimoNome = 'Mayworm';
+var SomaNomes = primeiroNome+' '+ultimoNome;
+
+// Coloque a seguinte frase em uma variável: It's time
+var fraseExerc = 'It\'s time';
+
+// Verifique o tipo da variável que contém o seu nome
+// console.log(typeof SomaNomes)
